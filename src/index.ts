d9c2c3f36e28bd676a5cc0b041d0ac0f86b445a1
Cameron Dykstra <cameron@resonint.com>
2022-06-03 14:56:23 +1200

    add command palette command to read current time

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
 
 /**
  * Initialization data for the jupyterlab_time_sync extension.
@@ -9,8 +13,22 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_time_sync',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_time_sync is activated!');
+
+    // Add an application command
+    const command: string = 'time_sync:run';
+    app.commands.addCommand(command, {
+      label: 'Resync',
+      execute: () => {
+        let dt = new Date();
+        console.log('Syncing Time:', dt);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Time Sync'});
   }
 };
 
